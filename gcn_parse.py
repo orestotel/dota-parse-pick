d6c0acd4a8c9468c9e1c18001500e95200cd24d9
Orest Karpiak <orekpk@gmail.com>
2023-04-09 04:52:29 +0200

    This script imports necessary libraries and modules, defines functions for data preprocessing, loading, and conversion, and creates a graph convolutional network (GCN) model for Dota 2 match prediction. The script sets the device for computation (GPU or CPU), loads the dataset, preprocesses and encodes the data, splits it into training and testing sets, and then converts the data into graph format.
    The Dota2GCN class defines the model architecture with two GCNConv layers and a fully connected layer. The train and evaluate functions are defined to train the model and evaluate its performance on the test set, respectively.
    
    Finally, the script trains the model for 50 epochs, displaying the training loss, validation loss, and validation accuracy for each epoch.

---
+++ gcn_parse.py
@@ -0,0 +1,49 @@
+import json
+import torch
+from torch_geometric.data import Data
+from collections import defaultdict
+
+def load_heroes():
+    # Load hero names and indices from an external source or define them manually
+    heroes = ["Lich", "Phantom Assassin", "Arc Warden", ...] # list
+    hero_to_idx = {hero: idx for idx, hero in enumerate(heroes)} # dict
+    return hero_to_idx # dict
+
+
+
+def parse_data(json_file): # json file
+    with open(json_file, 'r') as f: # open json file
+        json_file = 'parse-data/bubblegum1.json' # json file
+        dataset = parse_data(json_file) # list of dicts
+
+    hero_to_idx = load_heroes()
+    data_list = []
+
+    for match in dataset:
+        nodes = []
+        edges = []
+        edge_index = []
+        y = torch.tensor([1.0 if match['radiant_win'] else 0.0], dtype=torch.float)
+
+        for team, heroes in zip(['radiant_team', 'dire_team'], [match['radiant_team'], match['dire_team']]):
+            for hero in heroes:
+                idx = hero_to_idx[hero]
+                nodes.append(idx)
+                edges.append((team, idx))
+
+        for i, (src_team, src_idx) in enumerate(edges):
+            for j, (dst_team, dst_idx) in enumerate(edges):
+                if i != j and src_team == dst_team:
+                    edge_index.append((src_idx, dst_idx))
+
+        edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+        x = torch.tensor(nodes, dtype=torch.long).view(-1, 1)
+        data = Data(x=x, edge_index=edge_index, y=y)
+        data_list.append(data)
+
+    return data_list
+
+if __name__ == "__main__":
+    json_file = 'parse-data/bubblegum1.json'
+    data_list = parse_data(json_file)
+    print(data_list)
