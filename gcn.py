9cffdde34ec1ce3916bc9cc5182d095389f54d51
Orest Karpiak <orekpk@gmail.com>
2023-04-09 04:52:29 +0200

    This script imports necessary libraries and modules, defines functions for data preprocessing, loading, and conversion, and creates a graph convolutional network (GCN) model for Dota 2 match prediction. The script sets the device for computation (GPU or CPU), loads the dataset, preprocesses and encodes the data, splits it into training and testing sets, and then converts the data into graph format.
    The Dota2GCN class defines the model architecture with two GCNConv layers and a fully connected layer. The train and evaluate functions are defined to train the model and evaluate its performance on the test set, respectively.
    
    Finally, the script trains the model for 50 epochs, displaying the training loss, validation loss, and validation accuracy for each epoch.

--- gcn.py
+++ gcn.py
@@ -5,20 +5,26 @@ import torch.nn as nn
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import tkinter as tk
-from tkinter import ttk
 import os
 import pickle
+from gcn_parse import parse_data
+from datetime import datetime
 from tqdm import tqdm
 import sys
 import threading
 import time
 import glob
-import combine_files
-import training_module
 from torch_geometric.nn import GCNConv, global_mean_pool
 from torch_geometric.data import Data, DataLoader
+from torch_geometric.utils import to_networkx
+import networkx as nx  # For visualization
+import matplotlib.pyplot as plt  # For visualization
+from reportlab.lib.pagesizes import letter   # For PDF generation
+from reportlab.pdfgen import canvas  # For PDF generation
+from reportlab.lib import colors # For PDF generation
+from reportlab.lib.units import inch    # For PDF generation
 
+# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print(f"Using device: {device}")
@@ -53,9 +59,8 @@ stop_loading_animation = threading.Event()
 loading_thread = threading.Thread(target=loading_animation, args=(stop_loading_animation,))
 loading_thread.start()
 
-for filename in tqdm(dataset_filenames, desc="Loading datasets"):
-    with open(filename, "r") as file:
-        data.extend(json.load(file))
+json_file = 'parse-data/batches/bubblegum1.json' # Replace with your own path
+data = parse_data(json_file)
 
 # Stop loading animation
 stop_loading_animation.set()
@@ -64,29 +69,35 @@ sys.stdout.write("\r")
 sys.stdout.flush()
 
 # Preprocess and encode data
+# Collect all unique hero names
 heroes = set()
 for match in data:
     heroes |= set(match["radiant_team"]) | set(match["dire_team"])
 heroes = list(heroes)
+
+# Create a dictionary to map hero names to indices
 hero_to_idx = {hero: idx for idx, hero in enumerate(heroes)}
+
 
 def preprocess_data(data):
     X = []
     y = []
     for match in data:
-        radiant_team = [hero_to_idx[hero] for hero in match["radiant_team"]]
-        dire_team = [hero_to_idx[hero] for hero in match["dire_team"]]
+        radiant_team = match["radiant_team"]
+        dire_team = match["dire_team"]
         X.append((radiant_team, dire_team))
         y.append(match["radiant_win"])
     return X, y
+
 
 # Start loading animation
 stop_loading_animation = threading.Event()
 loading_thread = threading.Thread(target=loading_animation, args=(stop_loading_animation,))
 loading_thread.start()
 
-X, y = preprocess_data(data)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+train_size = int(len(data) * 0.8)
+test_size = len(data) - train_size
+train_data, test_data = torch.utils.data.random_split(data, [train_size, test_size])
 
 # Stop loading animation
 stop_loading_animation.set()
@@ -100,170 +111,95 @@ def team_to_graph_data(team):
     num_nodes = len(team)
     x = torch.tensor([hero_to_idx[h] for h in team], dtype=torch.long).view(-1, 1)
     edge_index = torch.tensor([(i, j) for i in range(num_nodes) for j in range(num_nodes) if i != j], dtype=torch.long)
-    edge_index = torch.cat([edge_index, torch.flip(edge_index, [0])], dim = 1)
+    edge_index = torch.cat([edge_index, torch.flip(edge_index, [0])], dim=1)
     return x, edge_index
 
-    # Convert
-    # the
-    # data
-    # into
-    #  PyTorch
-    #   Geometric
-    #    format
 
-    def to_pyg_data(X, y):
-        pyg_data = []
-
-    for (radiant_team, dire_team), radiant_win in zip(X, y):
+# Convert
+def convert_to_graph_data(X, y):
+    graph_data_list = []
+    for match, label in zip(X, y):
+        radiant_team, dire_team = match
         radiant_x, radiant_edge_index = team_to_graph_data(radiant_team)
-    dire_x, dire_edge_index = team_to_graph_data(dire_team)
-    data = Data(radiant_x=radiant_x, radiant_edge_index=radiant_edge_index,
-                dire_x=dire_x, dire_edge_index=dire_edge_index,
-                y=torch.tensor([radiant_win], dtype=torch.float))
-    pyg_data.append(data)
-    return pyg_data
+        dire_x, dire_edge_index = team_to_graph_data(dire_team)
+        graph_data = Data(radiant_x=radiant_x, radiant_edge_index=radiant_edge_index,
+                          dire_x=dire_x, dire_edge_index=dire_edge_index, y=label)
+        graph_data_list.append(graph_data)
+    return graph_data_list
 
-    #  Create
-    #   PyTorch
-    #  Geometric
-    #   datasets
-    train_data = to_pyg_data(X_train, y_train)
-    test_data = to_pyg_data(X_test, y_test)
 
-    #   Create
-    #    data
-    #    loaders
-    train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
+train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=16, shuffle=False)
 
-    #    Define
-    #  the
-    #    model
-
-    class Dota2GCN(nn.Module):
-        def init(self, in_channels, hidden_channels):
-
-            super(Dota2GCN, self).init()
-
-    self.conv1 = GCNConv(in_channels, hidden_channels)
-    self.conv2 = GCNConv(hidden_channels, hidden_channels)
-    self.fc = nn.Linear(2 * hidden_channels, 1)
+# Define the Dota2GCN model
+class Dota2GCN(nn.Module):
+    def __init__(self, num_heroes, hidden_channels):
+        super(Dota2GCN, self).__init__()
+        self.conv1 = GCNConv(1, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.fc = nn.Linear(hidden_channels * 2, 1)
 
     def forward(self, data):
         radiant_x, radiant_edge_index = data.radiant_x, data.radiant_edge_index
         dire_x, dire_edge_index = data.dire_x, data.dire_edge_index
 
-        # Pass the radiant team's graph through the GCN layers
         radiant_x = self.conv1(radiant_x, radiant_edge_index)
         radiant_x = torch.relu(radiant_x)
         radiant_x = self.conv2(radiant_x, radiant_edge_index)
         radiant_x = torch.relu(radiant_x)
-        radiant_x = global_mean_pool(radiant_x, torch.zeros(len(radiant_x), dtype=torch.long))
+        radiant_x = global_mean_pool(radiant_x, torch.zeros(radiant_x.size(0), dtype=torch.long))
 
-        # Pass the dire team's graph through the GCN layers
         dire_x = self.conv1(dire_x, dire_edge_index)
         dire_x = torch.relu(dire_x)
         dire_x = self.conv2(dire_x, dire_edge_index)
         dire_x = torch.relu(dire_x)
-        dire_x = global_mean_pool(dire_x, torch.zeros(len(dire_x), dtype=torch.long))
+        dire_x = global_mean_pool(dire_x, torch.zeros(dire_x.size(0), dtype=torch.long))
 
-        # Concatenate the team embeddings and pass through a final linear layer
-        out = torch.cat([radiant_x, dire_x], dim=1)
-        out = self.fc(out)
-        out = torch.sigmoid(out)
+        x = torch.cat([radiant_x, dire_x], dim=1)
+        x = self.fc(x)
+        x = torch.sigmoid(x)
+        return x.view(-1)
 
-        return out.squeeze(1)
-# Instantiate the model and optimizer
-model = Dota2GCN(1, 64).to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+model = Dota2GCN(len(heroes), 64).to(device)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.BCELoss()
 
-# Train the model
-training_module.train(model, train_loader, test_loader, optimizer, criterion, device, num_epochs=100)
+# Training function
+def train(model, loader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0
+    for data in loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        label = data.y.to(device, dtype=torch.float)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
 
-# Save the model
-torch.save(model.state_dict(), "model.pt")
-
-# Load the model
-model = Dota2GCN(1, 64).to(device)
-model.load_state_dict(torch.load("model.pt"))
-model.eval()
-
-def predict(model, radiant_team, dire_team):
-    radiant_x, radiant_edge_index = team_to_graph_data(radiant_team)
-    dire_x, dire_edge_index = team_to_graph_data(dire_team)
-    data = Data(radiant_x=radiant_x, radiant_edge_index=radiant_edge_index,
-                dire_x=dire_x, dire_edge_index=dire_edge_index)
-    data = data.to(device)
+# Evaluation function
+def evaluate(model, loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    correct = 0
     with torch.no_grad():
-        prediction = model(data)
-    return prediction.item()
+        for data in loader:
+            data = data.to(device)
+            output = model(data)
+            label = data.y.to(device, dtype=torch.float)
+            loss = criterion(output, label)
+            total_loss += loss.item()
+            pred = (output > 0.5).float()
+            correct += pred.eq(label.view_as(pred)).sum().item()
+    return total_loss / len(loader), correct / len(loader.dataset)
 
-# Example usage
-radiant_team = ["hero1", "hero2", "hero3", "hero4", "hero5"]
-dire_team = ["hero6", "hero7", "hero8", "hero9", "hero10"]
+# Set the number of training epochs
+num_epochs = 50
 
-win_probability = predict(model, radiant_team, dire_team)
-print(f"Radiant win probability: {win_probability * 100:.2f}%")
-
-# Tkinter GUI
-def get_heroes_list():
-    # Replace this with your actual list of heroes
-    return sorted(heroes)
-
-def on_submit():
-    radiant_team = [rt_var1.get(), rt_var2.get(), rt_var3.get(), rt_var4.get(), rt_var5.get()]
-    dire_team = [dt_var1.get(), dt_var2.get(), dt_var3.get(), dt_var4.get(), dt_var5.get()]
-
-    win_probability = predict(model, radiant_team, dire_team)
-    result_label.config(text=f"Radiant win probability: {win_probability * 100:.2f}%")
-
-# Create the main window
-root = tk.Tk()
-root.title("Dota 2 Match Predictor")
-
-# Radiant team
-rt_label = tk.Label(root, text="Radiant Team")
-rt_label.grid(row=0, column=0)
-
-rt_var1, rt_var2, rt_var3, rt_var4, rt_var5 = tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar()
-
-rt_cb1 = ttk.Combobox(root, textvariable=rt_var1, values=get_heroes_list(), state="readonly", width=20)
-rt_cb1.grid(row=1, column=0)
-rt_cb2 = ttk.Combobox(root, textvariable=rt_var2, values=get_heroes_list(), state="readonly", width=20)
-rt_cb2.grid(row=2, column=0)
-rt_cb3 = ttk.Combobox(root, textvariable=rt_var3, values=get_heroes_list(), state="readonly", width=20)
-rt_cb3.grid(row=3, column=0)
-rt_cb4 = ttk.Combobox(root, textvariable=rt_var4, values=get_heroes_list(), state="readonly", width=20)
-rt_cb4.grid(row=4, column=0)
-rt_cb5 = ttk.Combobox(root, textvariable=rt_var5, values=get_heroes_list(), state="readonly", width=20)
-rt_cb5.grid(row=5, column=0)
-
-# Dire team
-dt_label = tk.Label(root, text="Dire Team")
-dt_label.grid(row=0, column=1)
-
-dt_var1, dt_var2, dt_var3, dt_var4, dt_var5 = tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar()
-
-dt_cb1 = ttk.Combobox(root, textvariable=dt_var1, values=get_heroes_list(), state="readonly", width=20)
-dt_cb1.grid(row=1, column=1)
-dt_cb2 = ttk.Combobox(root, textvariable=dt_var2, values=get_heroes_list(), state="readonly", width=20)
-dt_cb2.grid(row=2, column=1)
-dt_cb3 = ttk.Combobox(root, textvariable=dt_var3, values=get_heroes_list(), state="readonly", width=20)
-dt_cb3.grid(row=3, column=1)
-dt_cb4 = ttk.Combobox(root, textvariable=dt_var4, values=get_heroes_list(), state="readonly", width=20)
-dt_cb4.grid(row=4, column=1)
-dt_cb5 = ttk.Combobox(root, textvariable=dt_var5, values=get_heroes_list(), state="readonly", width=20)
-dt_cb5.grid(row=5, column=1)
-
-# Submit button
-submit_button = tk.Button(root, text="Predict", command=on_submit)
-submit_button.grid(row=6, column=0, columnspan=2)
-
-# Result label
-result_label = tk.Label(root, text="")
-result_label.grid(row=7, column=0, columnspan=2)
-
-# Run the main loop
-root.mainloop()
-
+# Perform training and evaluation
+for epoch in range(num_epochs):
+    train_loss = train(model, train_loader, optimizer, criterion, device)
+    val_loss, val_acc = evaluate(model, test_loader, criterion, device)
+    print(f"Epoch: {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}")
